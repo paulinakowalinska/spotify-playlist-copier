8ca3b9bd39c205b9adf2755ba14497cc0f73f66f
paulinakowalinska <60118077+paulinakowalinska@users.noreply.github.com>
2021-01-25 17:21:45 +0100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,11 @@
+#OAuth token from Spotify API
+spotify_token=""
+#The developer's Spotify username
+spotify_user_id=""
+#ID of the playlist you want to parse
+spotify_playlist_id=""
+#Client ID and client secret from Spotify Console
+client_id = ""
+client_secret = ""
+#Number of tracks on the playlist to be copied (Spotify allows max.100 tracks)
+number_of_tracks=100
